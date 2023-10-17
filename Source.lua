721cbc9f80d3c88de4207f8e7b55871b2bb6f9ff
Nrazuke <95423741+Nrazuke@users.noreply.github.com>
2023-10-17 18:09:18 +0800

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -3337,14 +3337,7 @@ function ArrayFieldLibrary:CreateWindow(Settings)
 				end)
 				Button.Name = name
 				Button.Parent = PromptUI.Buttons -- saving memory
-				
-				-- Calculate the position to center the button horizontally.
-				local buttonWidth = Button.TextLabel.TextBounds.X + 24
-				local parentWidth = PromptUI.Buttons.AbsoluteSize.X
-				local xPosition = (parentWidth - buttonWidth) / 2
-				
-				Button.Position = UDim2.new(0, xPosition, 0, 0) -- Centered horizontally
-				Button.Size = UDim2.fromOffset(buttonWidth, 30
+				Button.Size = UDim2.fromOffset(Button.TextLabel.TextBounds.X + 24, 30)
 			end
 		end
 
